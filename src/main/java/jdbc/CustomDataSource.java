40b0dff17db16adcef1347a9ea617f8a7372f30a
MustafoyevMuhammad <110478566+MustafoyevMuhammad@users.noreply.github.com>
2023-06-03 17:56:57 +0500

    Update CustomDataSource.java

--- src/main/java/jdbc/CustomDataSource.java
+++ src/main/java/jdbc/CustomDataSource.java
@@ -5,9 +5,17 @@ import javax.sql.DataSource;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
 @Getter
 @Setter
 public class CustomDataSource implements DataSource {
+    private static final SQLException SQL_EXCEPTION = new SQLException();
     private static volatile CustomDataSource instance;
     private final String driver;
     private final String url;
@@ -15,11 +23,73 @@ public class CustomDataSource implements DataSource {
     private final String password;
 
     private CustomDataSource(String driver, String url, String password, String name) {
+        this.driver = driver;
+        this.url = url;
+        this.name = name;
+        this.password = password;
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Failed to load database driver: " + driver, e);
+        }
     }
 
     public static CustomDataSource getInstance() {
-
+        if (instance == null) {
+            synchronized (CustomDataSource.class) {
+                if (instance == null) {
+                    String driver = "org.postgresql.Driver";
+                    String url = "jdbc:postgresql://localhost:5432/myfirstdb";
+                    String name = "postgres";
+                    String password = "admin";
+                    instance = new CustomDataSource(driver, url, password, name);
+                }
+            }
+        }
         return instance;
+    }
+    @Override
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(url, name, password);
+    }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException {
+        return DriverManager.getConnection(url, username, password);
+    }
+    @Override
+    public PrintWriter getLogWriter() throws SQLException {
+        throw SQL_EXCEPTION;
+    }
+
+    @Override
+    public void setLogWriter(PrintWriter out) throws SQLException {
+        throw SQL_EXCEPTION;
+    }
+
+    @Override
+    public void setLoginTimeout(int seconds) throws SQLException {
+        throw SQL_EXCEPTION;
+    }
+
+    @Override
+    public int getLoginTimeout() throws SQLException {
+        throw SQL_EXCEPTION;
+    }
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        throw new SQLFeatureNotSupportedException();
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        throw SQL_EXCEPTION;
+    }
+
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        throw SQL_EXCEPTION;
     }
 
 }
